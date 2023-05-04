57b171899a63961897922d66609413de46328495
Muhammad51264 <muh.freijat@gmail.com>
2023-05-04 14:36:13 +0300

    task

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 let profiles = [
    {
         username: 'eva',
-        gender : 'female',
+        gender : 'Female',
         age: 24,
         birthday: '04 February 1990',
     },
@@ -9,13 +9,13 @@ let profiles = [
         username: 'Mohammad',
         gender : 'Male',
         age: 24,
-        birthday: '04 May 1990',
+        birthday: '04 May 1996',
     },
     {
         username: 'Majd',
         gender : 'Female',
         age: 24,
-        birthday: '04 May 1990',
+        birthday: '04 May 1999',
     },
     {
         username: 'Rawan',
@@ -29,18 +29,29 @@ let profilesContainer = document.getElementById('birthday__profile-container')
 let currentDate= new Date();
 let  month = currentDate.toLocaleString('default', { month: 'long' });
 let day = currentDate.getDay();
+let year = currentDate.getFullYear();
+let maleProfile=['assets/img/male.png','assets/img/male-2.png','assets/img/male-3.png']
+let femaleProfile=['assets/img/female.png','assets/img/female-2.png','assets/img/female-3.png']
 console.log(day)
 console.log(month);
+console.log(year);
 console.log(currentDate);
 for(let profile of profiles){
  let  birthday = profile.birthday.split(' ')
     if(birthday[1] === month && birthday[0] == day ){
     //Way 1 : The easy way
+         randompic=[];
+        if(profile.gender == "Male"){
+            randompic = maleProfile;
+            
+        }else{
+            randompic = femaleProfile;
+        }
         profilesContainer.innerHTML += ` <div class="birthday__profile">
-        <img src="assets/img/female.png" alt="">
+        <img src="${randompic[Math.floor(Math.random()*3)]}" alt="">
         <div class="birthday__info">
             <p>${profile.username}</p>
-            <p> ${profile.age}</p>
+            <p> ${year-parseInt(birthday[2],10)}</p>
         </div>
     </div>`
     //Way 2 : The hard way
@@ -62,4 +73,7 @@ for(let profile of profiles){
         //console.log(profile)
     }
 }
-   
+
+function clearAll(){
+    profilesContainer.innerHTML = ""
+}
